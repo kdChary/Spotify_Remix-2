27c67eeafe2ff4e0df881b5fa708d8e189deab39
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-04-05 04:03:50 +0000

    NotFoundPage created

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom'
+
 import './index.css'
 import BackBtn from '../BackBtn'
 
@@ -8,10 +10,15 @@ const NotFound = props => {
       <BackBtn history={history} />
       <img
         src="https://res.cloudinary.com/dgga8cymk/image/upload/v1712285974/1Spotify/404-sm.png"
-        alt="not-found"
+        alt="page not found"
         className="not-found-img"
       />
       <h1 className="not-found-title">Page Not Found</h1>
+      <Link to="/" className="link-item">
+        <button className="not-found-btn" type="button">
+          Home Page
+        </button>
+      </Link>
     </div>
   )
 }
